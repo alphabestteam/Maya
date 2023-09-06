647aa14f5dcd1abfbd14bffade3e695bfedd7d57
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-06 13:49:15 +0300

    fixing conflicts

--- Program.py
+++ Program.py
@@ -35,21 +35,5 @@ def main():
     print(prime_nums(152))
     print(prime_nums(60693))
 
-def calculate_factorial(chose_num):
-    num_factional = 1
-    for i in range(1, chose_num + 1):
-        num_factional = num_factional * i
-    return num_factional
-
-def main():
-    nums_sum = 0
-    for i in range(101):
-        nums_sum += i
-    print(nums_sum)
-    print(calculate_factorial(5))
-    print(calculate_factorial(6))
-    print(calculate_factorial(7))
-    print(calculate_factorial(8))
-
 if __name__ == "__main__":
     main()
