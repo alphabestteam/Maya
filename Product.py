80815ad7e2f311c6362d0c70495bffd8f284f898
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-10 19:04:07 +0300

    adding constructor to Product class

---
+++ Product.py
@@ -0,0 +1,23 @@
+class Product:
+    def __init__(self, product_name: str, product_price: float, product_units: int, total_product_price: float) -> None:
+        self._product_name = product_name
+        self._product_price = product_price
+        self._product_units = product_units
+        self._total_product_price = total_product_price
+    
+    @property
+    def product_name(self):
+        return self._product_name
+    
+    @property
+    def product_price(self):
+        return self._product_price
+    
+    @property
+    def product_units(self):
+        return self._product_units
+    
+    @property
+    def total_product_price(self):
+        return self._total_product_price
+    
