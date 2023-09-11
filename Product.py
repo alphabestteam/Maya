cef3528feb72fa3e84c02fe2eec52c3e8e7b4843
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-11 14:07:40 +0300

    fixing Product.py mistakes

--- Product.py
+++ Product.py
@@ -1,9 +1,9 @@
+import random
 class Product:
-    def __init__(self, product_name: str, product_price: float, product_units: int) -> None:
+    def __init__(self, product_name: str) -> None:
         self._product_name = product_name
-        self._product_price = product_price
-        self._product_units = product_units
-        self._total_product_price = self.product_price * self.product_units
+        self._product_price = random.uniform(1, 50)
+
     
     @property
     def product_name(self):
@@ -12,12 +12,5 @@ class Product:
     @property
     def product_price(self):
         return self._product_price
-    
-    @property
-    def product_units(self):
-        return self._product_units
-    
-    @property
-    def total_product_price(self):
-        return self._total_product_price
+
     
