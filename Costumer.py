283f6da7e68dcf8c2907a09193a40bb75bec3e5d
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-11 10:49:28 +0300

    adding remove_product function to Costumer class

--- Costumer.py
+++ Costumer.py
@@ -1,3 +1,4 @@
+from Product import Product
 class Costumer:
     def __init__(self, costumer_name: str) -> None:
         self._costumer_name = costumer_name
@@ -17,17 +18,40 @@ class Costumer:
         return self._total_price
     
 
-    def add_product(self, product_name: str, product_units: int) -> list:
+    def add_product(self, product_obj: Product) -> list:
         """
         a function that gets a product's info and adds it to the list of dictionaries
         """
         if self.groceries_list == []:
-            self.groceries_list.append({"name": product_name,  "units": int(product_units)})
+            self.groceries_list.append({"name": product_obj.product_name,  "units": int(product_obj.product_units)})
+            self._total_price += product_obj.total_product_price
             return self.groceries_list
         for dict in self.groceries_list:
-            if product_name in dict.values():
-                dict["units"] += int(product_units)
+            if product_obj.product_name in dict.values():
+                dict["units"] += int(product_obj.product_units)
             else:
-                new_dict = {"name": product_name,  "units": int(product_units)}
+                new_dict = {"name": product_obj.product_name,  "units": int(product_obj.product_units)}
                 self.groceries_list.append(new_dict)
+            self._total_price += product_obj.total_product_price #SEE IF CORRECT
             return self.groceries_list
+        
+        
+    def remove_product(self, product_obj: Product, delete_units: int) -> int:
+        """
+        a function that gets product's info and deletes what needed to be deleted if it makes sense
+        """
+        reduction = 0
+        for dict in self.groceries_list:
+            if product_obj.product_name not in dict:
+                raise ValueError('invalid name')
+            elif dict[product_obj.product_name]["units"] > delete_units:
+                dict[product_obj.product_name]["units"] -= delete_units
+                reduction = dict[product_obj.product_name]["price"] * delete_units
+                return reduction
+            elif dict[product_obj.product_name]["units"] == delete_units:
+                reduction = dict[product_obj.product_name]["price"] * delete_units
+                self.groceries_list.remove(dict)
+                return reduction
+            else:
+                raise ValueError('invalid input')
+        return reduction
