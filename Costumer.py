8de9546998ee9a7f39bfe0cf217277c2140a5d7c
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-11 14:07:15 +0300

    fixing mistakes in Costumer.py

--- Costumer.py
+++ Costumer.py
@@ -3,7 +3,6 @@ class Costumer:
     def __init__(self, costumer_name: str) -> None:
         self._costumer_name = costumer_name
         self._groceries_list = []
-        self._total_price = 0
 
     @property
     def costumer_name(self):
@@ -12,46 +11,44 @@ class Costumer:
     @property
     def groceries_list(self):
         return self._groceries_list
-    
-    @property
-    def total_price(self):
-        return self._total_price
-    
 
-    def add_product(self, product_obj: Product) -> list:
+
+    def total_price(self) -> float:
+        """
+        a function that returns the total price of a product for the costumer
+        """
+        for dict in self.groceries_list:
+            total_price = dict["units"] * dict["product"].product_price
+        return total_price
+
+
+    def add_product(self, product_obj: Product, product_units: int) -> None:
         """
         a function that gets a product's info and adds it to the list of dictionaries
         """
         if self.groceries_list == []:
-            self.groceries_list.append({"name": product_obj.product_name,  "units": int(product_obj.product_units)})
-            self._total_price += product_obj.total_product_price
-            return self.groceries_list
+            self.groceries_list.append({"product": product_obj, "units": int(product_units)})
+            return None
         for dict in self.groceries_list:
-            if product_obj.product_name in dict.values():
-                dict["units"] += int(product_obj.product_units)
-            else:
-                new_dict = {"name": product_obj.product_name,  "units": int(product_obj.product_units)}
-                self.groceries_list.append(new_dict)
-            self._total_price += product_obj.total_product_price #SEE IF CORRECT
-            return self.groceries_list
+            if product_obj.product_name == dict["product"].product_name:#SEE IF WORKS
+                dict["units"] += int(product_units)
+                return None
+        new_dict = {"product": product_obj, "units": int(product_units)}
+        self.groceries_list.append(new_dict)
         
-        
-    def remove_product(self, product_obj: Product, delete_units: int) -> int:
+
+    def remove_product(self, product_name: str, delete_units: int) -> bool:
         """
         a function that gets product's info and deletes what needed to be deleted if it makes sense
         """
-        reduction = 0
         for dict in self.groceries_list:
-            if product_obj.product_name not in dict:
-                raise ValueError('invalid name')
-            elif dict[product_obj.product_name]["units"] > delete_units:
-                dict[product_obj.product_name]["units"] -= delete_units
-                reduction = dict[product_obj.product_name]["price"] * delete_units
-                return reduction
-            elif dict[product_obj.product_name]["units"] == delete_units:
-                reduction = dict[product_obj.product_name]["price"] * delete_units
-                self.groceries_list.remove(dict)
-                return reduction
-            else:
-                raise ValueError('invalid input')
-        return reduction
+            if product_name == dict["product"].product_name:
+                if dict["units"] > delete_units:
+                    dict["units"] -= delete_units
+                    return True
+                elif dict["units"] == delete_units:
+                    self.groceries_list.remove(dict)
+                    return True
+                else:
+                    return False
+        return False
