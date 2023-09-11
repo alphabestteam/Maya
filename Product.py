598f7994b133114f032e2c08b21c2c71e7dc7465
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-11 10:52:59 +0300

    updating product class' attributes

--- Product.py
+++ Product.py
@@ -1,9 +1,9 @@
 class Product:
-    def __init__(self, product_name: str, product_price: float, product_units: int, total_product_price: float) -> None:
+    def __init__(self, product_name: str, product_price: float, product_units: int) -> None:
         self._product_name = product_name
         self._product_price = product_price
         self._product_units = product_units
-        self._total_product_price = total_product_price
+        self._total_product_price = self.product_price * self.product_units
     
     @property
     def product_name(self):
