12f67f5f18e0407451da4636617c74d10f761a30
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-13 14:26:55 +0300

    finishing generator questions

--- python_advanced_3.py
+++ python_advanced_3.py
@@ -13,8 +13,26 @@ def test_func(a, b):
     c = a + b
     return c / 100
 
+def return_sequence(N: int) -> int:
+    index_before = 2
+    index_two_before = 1
+    for index in range(1, N + 1):
+        if index == 1:
+            yield 1
+        elif index == 2:
+            yield 2
+        else: 
+            new_value = index_before * index_two_before
+            yield new_value
+            index_two_before = index_before
+            index_before = new_value
+
 def main():
     print(test_func())
 
+    for value in return_sequence(5):
+        print(value)
+
+        
 if __name__ == "__main__":
     main()
