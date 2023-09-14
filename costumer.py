1d49b7d834f90ff887c3ca1a83279ddedca56c7f
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-14 11:46:59 +0300

    adding costumer.py file

---
+++ costumer.py
@@ -0,0 +1,7 @@
+class Costumer:
+    def __init__(self, costumer_path) -> None:
+        self.costumer_name = "" #CHANGE CONSTRUCTOR
+        self.account_number = 0
+        self.current_balance = 0
+
+    
