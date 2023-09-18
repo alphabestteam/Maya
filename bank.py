3b665e75a98ca0fdf9747d2ce710fd645adc45e0
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-09-18 16:41:07 +0300

    finishing exercise

--- bank.py
+++ bank.py
@@ -2,27 +2,17 @@ from costumer_class import Costumer
 import socket, os, random, json
 COSTUMER_PATH = "costumers/"
 HOST = '127.0.0.1'
-PORT = 2221
+PORT = 2226
 MEMORY = 1024
 
 class Bank:
     def __init__(self) -> None:
-        self._costumers_list = self.get_costumers_list()
-    
-    @property
-    def costumers_list(self):
-        return self._costumers_list
-    
-
-    def get_costumers_list(self) -> list:
-        """
-        A function that creates a costumer's list from files in costumers folder
-        """
-        for costumer_file in os.scandir(COSTUMER_PATH):
-            new_costumer = Costumer(costumer_file.path)
-            self.costumers_list.append(new_costumer)
-        return self.costumers_list
-    
+        self.costumers_list = []
+        if os.path.exists(COSTUMER_PATH):
+            for costumer_file in os.listdir(COSTUMER_PATH):
+                if os.path.isfile(os.path.join(COSTUMER_PATH, costumer_file)) and costumer_file != ".DS_Store":
+                    new_costumer = Costumer(f"{COSTUMER_PATH}{costumer_file}")
+                    self.costumers_list.append(new_costumer)
 
     def start_listen(self):
         """
@@ -31,30 +21,27 @@ class Bank:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.bind((HOST, PORT))
         server_socket.listen()
+        print("waiting for connection")
         while True:
             conn, costumer_addr = server_socket.accept()
             print(f"server is connect by the address: {costumer_addr}")
             conn.sendall(bytes("Please enter 1: To create account\n2: To deposit or withdrawal money\n\
 3: To transfer money".encode('utf-8')))
-            costumer_data = conn.recv(MEMORY)
+            costumer_data = conn.recv(MEMORY).decode()
             if costumer_data == "1":
                 conn.sendall(bytes("Please enter your name and your current balance (name current-balance)\n".encode('utf-8')))
-                costumer_info = conn.recv(MEMORY)
+                costumer_info = conn.recv(MEMORY).decode()
                 self.create_account(costumer_info)
                 print("action was successful")
             elif costumer_data =="2":
-                conn.sendall(bytes("Would you like to deposit or withdraw money?".encode('utf-8')))
-                costumer_choice = conn.recv(MEMORY)
-                conn.sendall(bytes("Enter your name, your account number, and the amount of money you want to deposit or withdraw:\n".encode('utf-8')))
-                costumer_name, account_number, costumer_money = conn.recv(MEMORY).decode('utf-8').split(" ")
-                print(self.deposit_or_withdrawal(costumer_name, account_number, costumer_money, costumer_choice))
+                conn.sendall(bytes("Enter your name, your account number, the amount of money, and wether you want to deposit or withdraw :\n".encode('utf-8')))
+                costumer_choice = conn.recv(MEMORY).decode()
+                print(self.deposit_or_withdrawal(costumer_choice))
             elif costumer_data == "3":
                 conn.sendall(bytes("Please enter your information (name account number transaction money):\n".encode('utf-8')))
-                costumer_name, account_number, transaction_money = conn.recv(MEMORY).decode('utf-8').split(" ")
-                conn.sendall(bytes("Please enter the info for the person you want to transfer the money to (name account number ):\n".encode('utf-8')))
-                receiver_name, receiver_account = conn.recv(MEMORY).decode('utf-8').split(" ")
-                print(self.transfer_money(costumer_name, account_number, receiver_name, receiver_account, transaction_money))
-            if costumer_data == b"stop":
+                costumer_choice = conn.recv(MEMORY).decode()
+                print(self.transfer_money(costumer_choice))
+            elif costumer_data == b"stop":
                 break
         server_socket.close()
 
@@ -64,13 +51,14 @@ class Bank:
         A function that gets from costumer their info and creates a new file with it
         and adds it to the costumers' list as a costumer object
         """
-        costumer_name, costumer_balance = costumer_info.split(" ")
+        costumer_name, costumer_balance, _ = costumer_info.split(" ")
         costumer_account_num = self.new_account_number()
         with open(f"{COSTUMER_PATH}{costumer_account_num}", 'w') as new_costumer_file:
-            json.dumps({"name": costumer_name, "current balance": costumer_balance, 
-                                    "account number": costumer_account_num}, new_costumer_file)
+            json.dump({"name": costumer_name, "current balance": costumer_balance, "account number": costumer_account_num}, new_costumer_file, indent=4)
         new_costumer = Costumer(f"{COSTUMER_PATH}{costumer_account_num}")
-        self.costumer_list.append(new_costumer)
+        self.costumers_list.append(new_costumer)
+        for index in range(len(self.costumers_list)):
+            print(self.costumers_list[index].costumer_name)
 
 
     def new_account_number(self) -> int:
@@ -78,58 +66,77 @@ class Bank:
         A function that sets a random unique account number and returns it
         """
         while True:
-            account_number = random.randint(10000000, 100000000000)
-            for costumer in self.costumer_list:
+            account_number = random.randint(10000000, 999999999999)
+            for costumer in self.costumers_list:
                 if account_number == costumer.account_number:
                     continue
             return account_number
             
 
-    def deposit_or_withdrawal(self, costumer_name: str, account_number: int, costumer_money: float,
-                               costumer_action: str) -> str:
+    def deposit_or_withdrawal(self, costumer_choice: str) -> str:
             """
             A function that deposits or withdraws money from account if it's valid
             """
-            with open(f"{COSTUMER_PATH}{account_number}", 'w') as costumer_file:
+            costumer_name, account_number, costumer_money, costumer_action, _ = costumer_choice.split(" ")
+            with open(f"{COSTUMER_PATH}{account_number}", 'r') as costumer_file:
                 costumer_data = json.load(costumer_file)
-                if costumer_name == costumer_data["costumer name"]:
-                    if costumer_action.lower == "deposit":
-                        costumer_data["current balance"] += costumer_money
-                    elif costumer_action.lower == "withdraw":
-                        if costumer_data["current balance"] >= costumer_money:
-                            costumer_data["current balance"] -= costumer_money
+            with open(f"{COSTUMER_PATH}{account_number}", 'w') as costumer_file:
+                if not costumer_name == costumer_data["name"]:
+                    return "Given name doesn't match costumer's name on account"
+                else:
+                    if costumer_action.lower() == "deposit":
+                        costumer_data["current balance"] = float(costumer_data["current balance"]) + float(costumer_money)
+                    elif costumer_action.lower() == "withdraw":
+                        if float(costumer_data["current balance"]) >= float(costumer_money):
+                            costumer_data["current balance"] = float(costumer_data["current balance"]) - float(costumer_money)
                         else:
                             return "Not enough money in account for that action"
                     else:
                         return "Invalid input"
-                    json.dumps({"name": costumer_name, "current balance": costumer_data["current balance"], 
-                    "account number": account_number}, costumer_file)
-                    for costumer in self.costumer_list:
-                        if costumer.account_number == account_number:
-                            costumer.current_balance = costumer_data["current balance"]
+                    json.dump({"name": costumer_name, "current balance": costumer_data["current balance"], 
+                    "account number": account_number}, costumer_file, indent=4)
+                    for costumer in self.costumers_list:
+                        if int(costumer.account_number) == int(account_number):
+                            costumer.current_balance = float(costumer_data["current balance"])
                             return "Action was successful"
-                return "Given name doesn't match costumer's name on account"
+                
 
 
-    def transfer_money(self, costumer_name: str, costumer_account: int,
-                        receiver_name: str, receiver_account: int, transaction_money: float):
+    def transfer_money(self, costumer_choice: str) -> str:
         """
         A function that gets costumer's info and transfers money from their account to another person
         """
+        costumer_name, costumer_account, receiver_name, receiver_account, transaction_money, _ = costumer_choice.split(" ")
         with open(f"{COSTUMER_PATH}{costumer_account}", 'r') as costumer_file:
             costumer_data = json.load(costumer_file)
-            if costumer_name == costumer_data["costumer name"] and costumer_account == costumer_data["account number"]:
-                if costumer_data["current_balance"] < transaction_money:
+            if not (costumer_name == costumer_data["name"] and int(costumer_account) == int(costumer_data["account number"])):
+                return "Invalid input"  
+            else:
+                if float(costumer_data["current balance"]) < float(transaction_money):
                     return "Not enough money for this transaction"
-        with open(f"{COSTUMER_PATH}{receiver_account}", 'w') as receiver_file:
-            receiver_data = json.load(receiver_file)
-            if receiver_name == receiver_data["costumer name"] and receiver_account == receiver_account["account number"]:
-                receiver_data["current balance"] += transaction_money
-                for receiver in self.costumer_list:
-                    if receiver.account_number == receiver_account:
-                        receiver.current_balance = receiver_data["current balance"]
-                        return "Action was successful"
-        return "Invalid input"    
+                else:
+                    with open(f"{COSTUMER_PATH}{receiver_account}", 'r') as receiver_file:
+                        receiver_data = json.load(receiver_file)
+                    with open(f"{COSTUMER_PATH}{receiver_account}", 'w') as receiver_file:
+                        print(receiver_account, type(receiver_account))
+                        if not(receiver_name == receiver_data["name"] and int(receiver_account) == int(receiver_data["account number"])):
+                            return "Invalid input"  
+                        else:
+                            receiver_data["current balance"] = float(receiver_data["current balance"]) + float(transaction_money)
+                            with open(f"{COSTUMER_PATH}{costumer_account}", 'w') as costumer_file: #updates the balance of the costumer that made the transaction
+                                costumer_data["current balance"] = float(costumer_data["current balance"]) - float(transaction_money)
+                                json.dump({"name": receiver_name, "current balance": receiver_data["current balance"], 
+                    "account number": receiver_account}, receiver_file, indent=4)
+                                json.dump({"name": costumer_name, "current balance": costumer_data["current balance"], 
+                    "account number": costumer_account}, costumer_file, indent=4)
+                            for costumer in self.costumers_list:
+                                if int(costumer.account_number) == int(receiver_account):
+                                    costumer.current_balance = receiver_data["current balance"]
+                                if int(costumer.account_number) == int(costumer_account):
+                                    costumer.current_balance = costumer_data["current balance"] 
+    
+                    return "Action was successful"
+              
 
 def main():
     test_bank = Bank()
--- costumer_class.py
+++ costumer_class.py
@@ -2,23 +2,24 @@ import json
 class Costumer:
     def __init__(self, costumer_path) -> None:
         with open(f"{costumer_path}", "r") as costumer_file:
+            print(costumer_path)
             costumer_info = json.load(costumer_file)
-            self._costumer_name = costumer_info["name"]
-            self._account_number = costumer_info["account number"]
-            self._current_balance = costumer_info["current balance"]
+            self.costumer_name = costumer_info["name"]
+            self.account_number = costumer_info["account number"]
+            self.current_balance = costumer_info["current balance"]
 
-    @property
-    def costumer_name(self) -> str:
-        return self._costumer_name
+    # @property
+    # def costumer_name(self) -> str:
+    #     return self._costumer_name
       
-    @property
-    def account_number(self) -> int:
-        return self._account_number
+    # @property
+    # def account_number(self) -> int:
+    #     return self._account_number
 
-    @property
-    def current_balance(self) -> float:
-        return self._current_balance  
+    # @property
+    # def current_balance(self) -> float:
+    #     return self._current_balance  
 
-    @current_balance.setter
-    def current_balance(self, new_balance) -> None:
-        self.current_balance = new_balance
+    # @current_balance.setter
+    # def current_balance(self, new_balance) -> None:
+    #     self.current_balance = new_balance
