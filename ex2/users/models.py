18cf35f1af6e0d5b6671f2c1728959c064a29f6b
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-10-23 16:48:50 +0300

    adding forms, messages, and users models.py

---
+++ ex2/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.TextField(max_length=100)
+    last_name = models.TextField(max_length=100)
+    username = models.TextField(max_length=100)
+    user_id = models.IntegerField(MinValueValidator(0), primary_key=True)
+    email = models.TextField(max_length=100)
+    unread_messages = models.IntegerChoices(MinValueValidator(0))
