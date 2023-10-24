afd2e69a072386d38bff864b8d429504c7cd2153
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-10-24 10:58:43 +0300

    updating forms.models

--- ex2/forms/models.py
+++ ex2/forms/models.py
@@ -1,24 +1,29 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from users.models import User
+from chats.models import Chat
 
 # Create your models here.
 class Form(models.Model):
     opening_date = models.DateField()
     closing_date = models.DateField()
-    user_reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_reporter = models.ForeignKey(User,null=False, on_delete=models.CASCADE)
     status_options = (
         ('Open', 'Open'),
         ('Closed', 'Closed'),
         ('Waiting for Response', 'Waiting for Response'),
         ('Waiting for Handling', 'Waiting for Handling'),
     )
-    event_status = models.CharField(max_length=20, choices=status_options)
-    can_draft = models.BooleanField()
-    can_download = models.BooleanField()
+    event_status = models.CharField(max_length=100, choices=status_options)
+    can_draft = models.BooleanField(default=False)
+    can_download = models.BooleanField(default=False)
     shared_users = models.ManyToManyField(User, related_name='shared_events', blank=True)
 
-class SharedForm(models.Model):
+class SharedForm(Form):
     uploading_date = models.DateField()
-    uploading_user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField()
+    uploading_user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='forms/')
+
+class MessagesForm(Form):
+    chat_id = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    message_history = models.JSONField()
