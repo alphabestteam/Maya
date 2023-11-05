0aaa530c985e5cc8069b4d9cc951739563e7f6c2
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-11-05 11:59:34 +0200

    answering question a

---
+++ confirm_prompt_alert.js
@@ -0,0 +1,13 @@
+/*-------JAVASCRIPT EXERCISE 1--------
+a. alert:
+alert() is a popup window when you do something that activates the alert() method.
+so for example if you click on a button and the button function has alert method in it, when you click on it a
+box is going to popup with a message on it.
+prompt:
+prompt() is very similar to alert, it also activates a popup box. However the difference is that the popup box it opens, 
+has a place for the user to add an input. so for example when a prompt() method is activated, a popup box opens with an option to write a message (such as name or anything)
+and the function gets the user's input and performs a certain action with it(so for example writing back a message that includes the user's input).
+confirm:
+a confirm() method also opens a popup box, but with the options of buttons cancel and OK. so the user can choose which button to click and it will act accordingly.
+Mostly we use confirm() for validation or asking the user to accept or decline something.*/
+
