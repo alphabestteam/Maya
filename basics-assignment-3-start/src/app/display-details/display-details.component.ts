0a148575a3c6fb204c2e3403d3fcbfd3992bd081
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-11-20 14:26:50 +0200

    finishing exercise 3

--- basics-assignment-3-start/src/app/display-details/display-details.component.ts
+++ basics-assignment-3-start/src/app/display-details/display-details.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-display-details',
   templateUrl: './display-details.component.html',
-  styleUrl: './display-details.component.css'
+  styleUrls: ['./display-details.component.css']
 })
-export class DisplayDetailsComponent {
 
+export class DisplayDetailsComponent {
+  details: boolean = true;
+  counter: number = 0;
+
+  displayDetails() {
+    const counterElement = document.createElement('p');
+    const displayArea = document.getElementById('clickCounter');
+    const detailsElement = document.getElementById('details');
+    if (this.details){
+      detailsElement.style.visibility = 'hidden';
+      this.details = false;
+    }
+    else{
+      this.details = true;
+      detailsElement.style.visibility = 'visible';
+    }
+    this.counter++;
+
+    counterElement.textContent = `${this.counter}`;
+    counterElement.style.margin = '0';
+
+    if (this.counter >= 5){
+      counterElement.style.backgroundColor = "blue";
+    }
+    if (displayArea) {
+      displayArea.appendChild(counterElement);
+    }
+  };
 }
