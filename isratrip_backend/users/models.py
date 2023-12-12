eee5557382bfe154e15a77374a2aae731d828414
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-12-12 16:34:37 +0200

    finishing basics for users app

--- isratrip_backend/users/models.py
+++ isratrip_backend/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+ROLE_OPTIONS = (
+    ('user', 'user'),
+    ('admin', 'admin'),
+)
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True, editable=False)
+    first_name = models.TextField(max_length=100)
+    last_name = models.TextField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    email = models.EmailField()
+    city = models.TextField()
+    date_of_birth = models.DateField()
+    role = models.TextChoices(choices=ROLE_OPTIONS)
