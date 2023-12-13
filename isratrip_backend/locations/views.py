2fec617ee469e671e6c18c70135bbb15ffee1371
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-12-13 09:25:25 +0200

    finishing basic functions for locations app

--- isratrip_backend/locations/views.py
+++ isratrip_backend/locations/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from .models import Location
+from .serializers import LocationSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+
 # Create your views here.
-@csrf_exempt
-def get_people(request):
-    """
-    a function that gets all people from db
-    """
-    if request.method == 'GET':
-        persons = Location.objects.all()
-        serializer = LocationSerializer(persons, many=True)
+class ListLocations(APIView):
+    def get(self, request):
+        """
+        a function that gets all locations from db
+        """
+        locations = Location.objects.all()
+        serializer = LocationSerializer(locations, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+        
+    def create(self, request):
+        """
+        a function that add a location to db
+        """
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
