cf58e2759383da3497ecf41deaca29f58ee66efc
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-12-14 17:16:32 +0200

    adding register component

--- isratrip-frontend/src/app/app.module.ts
+++ isratrip-frontend/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { RegisterFormComponent } from './register-form/register-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- isratrip-frontend/src/app/register-form/register-form.component.ts
+++ isratrip-frontend/src/app/register-form/register-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.css']
 })
-export class RegisterFormComponent {
+export class RegisterFormComponent implements OnInit {
+  registerForm!: FormGroup;
+  formSubmitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      first_name: ['', [Validators.required]],
+      last_name: ['', [Validators.required]],
+      date_of_birth: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.formSubmitted = true;
+    if (this.registerForm.invalid) {
+        return;
+    }
+    else{
+      console.log(this.registerForm.value);
+    }
+  }
 }
