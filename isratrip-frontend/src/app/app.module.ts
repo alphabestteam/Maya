6d56aae2784fe71a9618964d0172746badac2926
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-12-14 13:42:49 +0200

    openning new angular project

---
+++ isratrip-frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
